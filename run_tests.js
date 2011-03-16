b00cc3cad3a4dc97b51f5783c981995dd39b783e
Carl Fürstenberg <azatoth@gmail.com>
2011-03-16 20:23:35 +0100

    Adding copy of jQuery core unit tests
    Copying most of the jQuery core unit tests (found in
    jquery/test/unit/core.js). What is not included are tests require ajax
    to work, and tests that depends on working iframe (I'm not certain how
    to handle iframes)
    
    Currently all but two tests from jQuery core unit tests passes, the two
    that doesn't work I've added two more tests which focus on $().text()
    which doesn't seems to work at the moment fully.

---
+++ run_tests.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+try {
+    var reporter = require('nodeunit').reporters.default;
+}
+catch(e) {
+    var sys = require('sys');
+    sys.puts("Cannot find nodeunit module.");
+    sys.puts("You can download submodules for this project by doing:");
+    sys.puts("");
+    sys.puts("    git submodule init");
+    sys.puts("    git submodule update");
+    sys.puts("");
+    process.exit();
+}
+
+process.chdir(__dirname);
+reporter.run(['test']);
+
