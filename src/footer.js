da2d14e32a441ab5d0cf57354a3f1f05c48a2703
James Morrin <treasonx@gmail.com>
2012-04-28 12:29:21 -0400

    consolidated into wrapper

--- src/footer.js
+++
@@ -1,9 +0,0 @@
-
-  // begin npm / ender footer
-  window.jQuery.noConflict();
-  return window.jQuery;
-}
-module.exports = create('undefined' === typeof window ? undefined : window);
-module.exports.create = create;
-}());
-
