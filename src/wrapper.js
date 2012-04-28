e80349d2ffaf105849adb621b6eeb15286a958aa
James Morrin <treasonx@gmail.com>
2012-04-28 12:34:05 -0400

    wrapper template for node-jquery

---
+++ src/wrapper.js
@@ -0,0 +1,20 @@
+(function () {
+function create(window) {
+  var location, navigator, XMLHttpRequest;
+
+  window = window || require('jsdom').jsdom().createWindow();
+  location = window.location || require('location');
+  navigator = window.navigator || require('navigator');
+
+  if (!window.XMLHttpRequest && 'function' !== typeof window.ActiveXObject) {
+    window.XMLHttpRequest = require('xmlhttprequest');
+  }
+
+  //JQUERY_SOURCE
+
+  window.jQuery.noConflict();
+  return window.jQuery;
+}
+module.exports = create('undefined' === typeof window ? undefined : window);
+module.exports.create = create;
+}());
