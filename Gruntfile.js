d20b124be37bdf67c24e9e48424241cb22bf3a89
Aditya <aditya@netroy.in>
2013-04-02 22:29:33 +0200

    updated Gruntfile correctly for 0.4.. watch works again

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,30 @@
 module.exports = function(grunt) {
 
+  // Project configuration.
+  grunt.initConfig({
+    'pkg': grunt.file.readJSON('package.json'),
+    'nodeunit': {
+      'all': ['test/*.js']
+    },
+    'watch': {
+      'files': '<%= jshint.all %>',
+      'tasks': 'jshint',
+      'options': {
+        'debounceDelay': 250
+      }
+    },
+    'jshint': {
+      'all': ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+      'options': {
+        'jshintrc': '.jshintrc'
+      }
+    }
+  });
+
   // Load grunt plugins
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    nodeunit: {
-      all: ['test/*.js']
-    },
-    watch: {
-      files: '<config:jshint.files>',
-      tasks: 'default'
-    },
-    jshint: {
-      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    }
-  });
 
   // Register tasks.
   grunt.registerTask('default', ['jshint', 'nodeunit']);
